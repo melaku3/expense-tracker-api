002c77397c9ca6399f8d82c8d0458519792a3c26
melaku3 <melaku.aweke@astu.edu.et>
2025-02-27 23:26:03 +0300

    Enhance category model with user association, type, and color fields

--- models/categoryModel.ts
+++ models/categoryModel.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
     name: { type: String, required: true, unique: true, trim: true },
-    description: { type: String, default: '' }
+    description: { type: String, default: '' },
+    type: {type: String, enum: ['income', 'expense'], required: true},
+    colorCode: {type: String, default: '#000000'}
+
 }, { timestamps: true });
 
 const categoryModel = mongoose.models.category || mongoose.model('category', categorySchema);
